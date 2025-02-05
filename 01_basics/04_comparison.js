f6427ebbf3cf60cce3a72b62fa887a2ce8feff37
ChandanP07 <chandan019283@gmail.com>
2025-02-05 19:13:48 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);          // equality check
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);    // false for all 3
+// console.log(undefined < 0);
+
+
+// ===
+console.log("2" == 2);
+
+
+
