3f1519e2053095e4511e93e3d14ee2b692a3ed08
ChandanP07 <chandan019283@gmail.com>
2025-02-05 22:19:00 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,45 @@
+const name = "chandan";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and repo count is ${repoCount}`);
+
+const gameName = 'Batman is the game of the year 2023.';
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("t"));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+// const newStringOne = "   chandan   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url="https://www.youtube.com/watch%20?v=12345";
+
+// console.log(url.replace('%20','-'))
+
+// console.log(url.includes('chandan'))
+
+// console.log(gameName.split('-'))
+
+// const content="TOny Stark"
+// console.log(content.bold())
+
+const index=4
+console.log(
+    `Character code ${gameName.charCodeAt(index)} is equal to ${gameName.charAt(
+    index,
+)}`,
+);
