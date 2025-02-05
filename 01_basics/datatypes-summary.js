c6275f6050e19234e4ef9ed9b83fb94e27d67d60
ChandanP07 <chandan019283@gmail.com>
2025-02-05 20:16:01 +0530

    datatype-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,32 +3,58 @@
 // 7 types: String, Number, Boolean, null, Undefined, Symbol, BigInt
 
 const score = 100; // ==> number
-const scoreValue = 100.5;       // ==> number
+const scoreValue = 100.5; // ==> number
 
-const isLoggedIn = false;       // ==> boolean
-const outsideTemp = null;       // ==> null
+const isLoggedIn = false; // ==> boolean
+const outsideTemp = null; // ==> null
 let userEmail;
 
-const id = Symbol("123");       // ==> symbol
+const id = Symbol("123"); // ==> symbol
 const anotherid = Symbol("123");
 
 // console.log(id === anotherid);   // false
 
-const bigNumber = 235465721326589844561n;    // ==> bigint
+const bigNumber = 235465721326589844561n; // ==> bigint
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["batman", "superman", "wonderwoman"];  // ==> object
+const heros = ["batman", "superman", "wonderwoman"]; // ==> object
 
-let myObj = {           // ==> object
+let myObj = {
+  // ==> object
   name: "John",
   age: 30,
 };
 
-const myFunction = function () {    // ==> function 
+const myFunction = function () {
+  // ==> function
   console.log("Hello World");
 };
 
 console.log(typeof myFunction);
+
+// ******************* Memory *********************
+
+// Stack (Primitive),Heap(Non-Primitive)
+
+let myYoutubename = "hiteshchoudhary";
+
+let anothername = myYoutubename;
+anothername = "chandan";
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+  email: "user@email.com",
+  upi: "user@sbi",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "chandan@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
