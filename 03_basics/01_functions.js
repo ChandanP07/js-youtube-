ad7416b3b9d0b28b2410d46732c7029976da42fa
ChandanP07 <chandan019283@gmail.com>
2025-03-30 13:59:52 +0530

    basicsfunction01

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function sayMyName() {
+  console.log("H");
+  console.log("i");
+  console.log("t");
+  console.log("e");
+  console.log("s");
+  console.log("h");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2) {
+//   console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2) {
+//   let result = number1 + number2;
+//   return result;
+    return number1 + number2
+}
+const result = addTwoNumbers(3, 5);
+
+console.log(result);
